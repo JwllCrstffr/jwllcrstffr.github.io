ec8ef1441ac89432af6bebe2a32ec0eac73a5488
JwllCrstffr <jowellcristoffer@gmail.com>
2024-09-18 14:43:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+
+
+//Typewriter Effect //
+
+const texts = [
+    "Web Developer.",
+    "Programmer.",
+    "Gamer.",
+    "Dancer.",
+    "Singer.",
+    "Kutingtingero."
+]
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex)
+        charcterIndex++;
+        setTimeout(typeWriter, speed)
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
+
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1)
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter,500)
+    }
+}
+
+window.onload = typeWriter
